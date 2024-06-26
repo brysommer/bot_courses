f96a5f72146efb4a2b13cd59c64656b7c615ad9a
brysommer <brysommer1@gmail.com>
2024-06-26 16:06:36 +0300

    new ip

--- wfpinit.js
+++ wfpinit.js
@@ -6,9 +6,9 @@ WFP_CONFIG.DEFAULT_PAYMENT_CURRENCY = 'UAH';
 export const wfp = new WFP({
     MERCHANT_ACCOUNT: 'itgin_online',
     MERCHANT_SECRET_KEY: values.merchant_sercret,
-    MERCHANT_DOMAIN_NAME: '172.31.38.9',
+    MERCHANT_DOMAIN_NAME: '51.20.1.118:3000',
     // service URL needed to receive webhooks
-    SERVICE_URL: `172.31.38.9'/webhook`,
+    SERVICE_URL: `51.20.1.118:3000/webhook`,
 });
 
 
